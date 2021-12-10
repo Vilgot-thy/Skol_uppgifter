94b54bc567f36933513583b3e5d719e8abd8889c
Vilgot-thy <92026267+Vilgot-thy@users.noreply.github.com>
2021-12-10 13:19:49 +0100

    Add files via upload

--- Skitgubbe_1.py
+++ Skitgubbe_1.py
@@ -1,9 +1,50 @@
 import time
 import random
+from typing import Union
 
 pengar = 100
+namn = ""
+
+def AI_spelar():    
+    Kort = random.choice(AI_kort_i_han)
+    Spelade_kort.append(Kort)
+    for x in AI_kort_i_han:
+        if Kort == x:
+            L = AI_kort_i_han.index(x)
+    AI_kort_i_han.pop(L)
+    AI_kort_i_hand_farg.pop(L)
+
+    if len(Kort_lek) != 0:
+        if len(AI_kort_i_han) < 3:
+            kort = random.choice(Kort_lek)
+            AI_kort_i_han.append(kort)
+            Kort_lek.remove(kort)
+            Kort_farg = random.choice(Kort_lek_farg)
+            AI_kort_i_hand_farg.append(Kort_farg)
+            Kort_lek_farg.remove(Kort_farg)
+
+
+def beting():
+    global pengar
+    u = 1
+    while u == 1:
+        print(f"Hur mycket vill du beta?\nDU har {pengar} pengar kvar")
+        beting = int(input(""))
+        if beting > pengar:
+            print("Du har inte tilräkligt mycke pengar")
+            time.sleep(3)
+        else:
+            pengar = pengar - beting
+            u += 1
+            time.sleep(3)
+
+
 
 def Skitgubbe():
+    global pengar
+    global namn
+    global AI_kort_i_han, Spelade_kort, AI_kort_i_hand_farg, Kort_siffra_list, Kort_lek, Kort_lek_farg
+    u = 1
     o = 0
     k = 0
     match = 0
@@ -22,15 +63,17 @@ def Skitgubbe():
     AI_kort_på_bord_gömda_farg = []
     Kort_lek = []
     Kort_lek_farg = []
-    Kort_siffra_list = {"14", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13"}
-    Kort_farg_list = {"♥", "♦", "♣", "♠", "♥", "♦", "♣", "♠", "♥", "♦", "♣", "♠", "♥", "♦", "♣", "♠"}
-    
-    while o != 4:
-        for n in (Kort_siffra_list):
-            Kort_lek.append(n)
+    Kort_siffra_list = ["14", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13"]
+    Kort_farg_list = ["♥", "♦", "♣", "♠"]
+
+
+
+    while o != 52:
+        kort = random.choice(Kort_siffra_list)
+        Kort_lek.append(kort)
+        Kort_farg = random.choice(Kort_farg_list)
+        Kort_lek_farg.append(Kort_farg)
         o += 1
-        for f in (Kort_farg_list):
-            Kort_lek_farg.append(f)
 
     while k != 3:
         kort = random.choice(Kort_lek)
@@ -66,45 +109,36 @@ def Skitgubbe():
         Kort_farg = random.choice(Kort_lek_farg)
         AI_kort_på_bord_farg.append(Kort_farg)
         Kort_lek_farg.remove(Kort_farg)
-        print(Kort_lek_farg)
         Kort_farg = random.choice(Kort_lek_farg)
         AI_kort_på_bord_gömda_farg.append(Kort_farg)
         Kort_lek_farg.remove(Kort_farg)
         Kort_farg = random.choice(Kort_lek_farg)
         AI_kort_i_hand_farg.append(Kort_farg)
         Kort_lek_farg.remove(Kort_farg)
-
         k += 1
-    print(Kort_i_Hand)
-    print(Kort_på_bord_gömda)
-    print(Kort_på_bord)
 
-    print(Kort_i_Hand_farg)
-    print(Kort_på_bord_farg)
-    print(Kort_på_bord_gömda_farg)
 
-    print(AI_kort_i_han)
-    print(AI_kort_på_bord)
-    print(AI_kort_på_bord_gömda)
-
-    while match == 0:
-        print(f""" Nu är matchen i gång
-                  Du kan se dina längst ner och motståndarens kort längst upp  
-
-                                    Måtståndare:
-                                      Påbordet:
-                            {AI_kort_på_bord_farg[0],AI_kort_på_bord[0]} {AI_kort_på_bord_farg[1],AI_kort_på_bord[1]} {AI_kort_på_bord_farg[2],AI_kort_på_bord[2]}
+    
+    print(f"Hej I detta rumet kommer vi köra några omgångar av skitgubbe MED MIN REGLER!")
+    time.sleep(3)
+    print(f"Det som är anorlunda med mina regler är\n1. korten har inga spesiela egenskaper")
+    print(f"2. Läger du fyra av sama så vänds inte högen bort")
+    print(f"3. Jag börjar altid")
 
 
 
-                        
-                  """)
-        match += 1
+
+    print(f"Dina kort i din hand är {Kort_i_Hand_farg[0]}{Kort_i_Hand[0]} {Kort_i_Hand_farg[1]}{Kort_i_Hand[1]} {Kort_i_Hand_farg[2]}{Kort_i_Hand[2]}")
+    print(f"Dina kort på bordet är {Kort_på_bord_farg[0]}{Kort_på_bord[0]} {Kort_på_bord_farg[1]}{Kort_på_bord[1]} {Kort_på_bord_farg[2]}{Kort_på_bord[2]}")
+    time.sleep(2)
+    print(f"\nDina motstådares kort på bordet är {AI_kort_på_bord_farg[0]}{AI_kort_på_bord[0]} {AI_kort_på_bord_farg[1]}{AI_kort_på_bord[1]} {AI_kort_på_bord_farg[2]}{AI_kort_på_bord[2]}")
+
+    beting()
+
+    AI_spelar()
 
 
-
-    print(Kort_i_Hand)
-    print(Kort_på_bordet_gömda)
-    print(Kort_på_bordet)
+    
+    
 
 Skitgubbe()
